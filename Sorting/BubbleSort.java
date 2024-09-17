38df78d1e13bd0684dd72b20f803429a2832ead3
avantika-14 <avapoojapoonam14@gmail.com>
2024-09-17 08:50:39 +0530

    bubble and selection

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -8,18 +8,21 @@ public class BubbleSort {
     }
     public static void bubble(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            boolean swap = false;
+            boolean swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                    swap = true;
+                    swap(arr, j, j - 1);
+                    swapped = true;
                 }
             }
-            if (!swap) {
+            if (!swapped) {
                 break;
             }
         }
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4,5,3,21,2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int larg = arr.length - i -1;
+            int maxIndex = getMaxIndex(arr,0,larg);
+            swap(arr,maxIndex,larg);
+        }
+    }
+    public static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
