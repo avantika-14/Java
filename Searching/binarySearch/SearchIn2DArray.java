b9bab344e53712ef90c57704d75d9beade0488f5
avantika-14 <avapoojapoonam14@gmail.com>
2024-09-19 20:03:03 +0530

    2d

---
+++ Searching/binarySearch/SearchIn2DArray.java
@@ -0,0 +1,28 @@
+package binarySearch;
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int[][] mat= {
+            {10, 20, 30, 40},
+            {15, 25, 35, 45},
+            {28, 29, 37, 49},
+            {33, 34, 38, 50}
+        };
+        int target = 37;
+        System.out.println(Arrays.toString(search(mat, target)));
+    }
+    static int[] search(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix.length - 1;
+        while(row < matrix.length && col >= 0) {
+            if(matrix[row][col] == target)
+                return new int[]{row,col};
+            if(matrix[row][col] > target)
+                col--;
+            else
+                row++;
+        }
+        return new int[]{-1,-1};
+    }
+}
