492939c9d7902b2f3bd5dce6e7550bc6051698be
avantika-14 <avapoojapoonam14@gmail.com>
2025-05-30 15:58:47 +0530

    done

---
+++ Practice Interview Questions/Isogram.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Isogram {
+
+    public static boolean isIsogram(String s) {
+
+        boolean[] check = new boolean[26];
+
+        for (char ch : s.toLowerCase().toCharArray()) {
+
+            if (ch >= 'a' && ch <= 'z') {
+
+                int index = ch - 'a';
+
+                if (check[index])
+                    return false;
+
+                check[index] = true;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        System.out.println("Isogram? " +isIsogram(s));
+    }
+}
