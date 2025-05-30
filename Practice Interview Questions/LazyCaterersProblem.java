d1531a5111272ad0a3e762672b415ecf8ed273a4
avantika-14 <avapoojapoonam14@gmail.com>
2025-05-30 15:07:10 +0530

    done

---
+++ Practice Interview Questions/LazyCaterersProblem.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LazyCaterersProblem {
+
+    public static int maximumPieces (int n){
+        return (int)(long) n * (n + 1) / 2 + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number of cuts in the cake:");
+        int n = sc.nextInt();
+
+        System.out.println("the maximum number of pieces in the cake: " +maximumPieces(n));
+    }
+}
