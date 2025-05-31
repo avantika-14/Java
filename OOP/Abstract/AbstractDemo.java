2b699a45cedff87a23a1d4aa0836db7b488cf461
avantika-14 <avapoojapoonam14@gmail.com>
2025-05-31 22:33:23 +0530

    done

--- OOP/Abstract/AbstractDemo.java
+++ OOP/Abstract/AbstractDemo.java
@@ -4,10 +4,16 @@ public class AbstractDemo {
 
     public static void main(String[] args) {
 
-        Employee contractor = new Contractor("contractor A", 10, 10);
-        System.out.println(contractor.calculatePay());
+        // employee are of two types - contractual and full time
+        // here FullTimeEmployee and Contractor class extend Employee to show the same categories
 
+        Employee contractor = new Contractor("contractor A", 10, 10);
         Employee fullTimeEmployee = new FullTImeEmployee("Employee F", 20);
+
+        // the abstraction concept lies here in the caculatePay where it's called in its own type of class' version
+        // here only the required output is shown, not how its calculated
+        // and the calculations are objective to the type of employee and not defined in the Employee class
+        System.out.println(contractor.calculatePay());
         System.out.println(fullTimeEmployee.calculatePay());
     }
 }
