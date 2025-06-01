d80d9db1ac25ef651658d1243542535635abba67
avantika-14 <avapoojapoonam14@gmail.com>
2025-06-01 12:29:27 +0530

    done

---
+++ OOP/AccessModifiers/p1/Public.java
@@ -0,0 +1,8 @@
+package AccessModifiers.p1;
+
+public class Public {
+
+    public void publicMethod() {
+        System.out.println("Public method");
+    }
+}
---
+++ OOP/AccessModifiers/p2/PublicOne.java
@@ -0,0 +1,11 @@
+package AccessModifiers.p2;
+import AccessModifiers.p1.Public;
+
+public class PublicOne {
+
+    public static void main(String[] args) {
+
+        Public po = new Public();
+        po.publicMethod();
+    }
+}
