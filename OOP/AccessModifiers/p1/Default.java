1c146bec415b87ca04a939c1aececc25b439ae2b
avantika-14 <avapoojapoonam14@gmail.com>
2025-06-01 12:05:10 +0530

    done

---
+++ OOP/AccessModifiers/p1/Default.java
@@ -0,0 +1,8 @@
+package AccessModifiers.p1;
+
+class Default {  // not public
+//    void display(){
+//        System.out.println("Default method");
+//    }
+}
+
---
+++ OOP/AccessModifiers/p2/DefaultOne.java
@@ -0,0 +1,13 @@
+package AccessModifiers.p2;
+//import AccessModifiers.p1.*;
+//
+class DefaultOne {
+    //
+    public static void main(String[] args) {
+//
+//        Default d = new Default(); // hence doesnt show up here
+//        d.display();
+//        // hence will show error that it cannot find default class, so its necessary to have it public
+    }
+}
+
